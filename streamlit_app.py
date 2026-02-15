7cb3f038818768aea4674382c9d26ba7cfa2f713
Milena Molotoka <milena.molotoka@cellularsales.com>
2026-02-15 11:07:15 -0500

    Update SQL insert statement to include name_on_order

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,10 +46,12 @@ if ingredients_list:
 
     #st.write(ingredients_string)
     
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-        values ('""" + ingredients_string + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+        values ('""" + ingredients_string + """','"""+name_on_order +"""')"""
+#st.write(my_insert_stmt)
+#st.stop()  
     time_to_insert = st.button('Submit Order')
-    #st.write(my_insert_stmt)
+
     if time_to_insert:
         session.sql(my_insert_stmt).collect()    
         st.success('Your Smoothie is ordered!', icon="✅")
